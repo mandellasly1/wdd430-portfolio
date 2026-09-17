3552f8a6dad16ba93f551cfe8f38931d05c7f25b
mandellasly1 <akpomah_nelson@yahoo.com>
2026-09-17 08:52:17 +0100

    Fix API route for projects by ID

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ turbopack: {
+    root: __dirname, // treat this folder as the root
+  },
 };
 
 export default nextConfig;
